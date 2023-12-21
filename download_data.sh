7ae2425926bf21152b305fd28c160bd5e592ddcf
LH104729 <lh104729@gmail.com>
2023-12-21 12:16:41 +0800

    LFS untrack

---
+++ download_data.sh
@@ -0,0 +1,3 @@
+wget https://github.com/xlxwalex/FCGEC/raw/main/data/FCGEC_train.json -P ./data/
+wget https://github.com/xlxwalex/FCGEC/raw/main/data/FCGEC_test.json -P ./data/
+wget https://github.com/xlxwalex/FCGEC/raw/main/data/FCGEC_valid.json -P ./data/
