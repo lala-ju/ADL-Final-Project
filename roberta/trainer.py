332cc1263d800f28214821f33012c61647f0e78c
LeeYenCHieh <B10902113@ntu.edu.tw>
2023-12-23 03:53:53 +0800

    debug

--- roberta/trainer.py
+++ roberta/trainer.py
@@ -459,10 +459,10 @@ def main():
         inputs = examples[text_column]
         targets = examples[summary_column]
         inputs = [prefix + inp for inp in inputs]
-        model_inputs = tokenizer(inputs, max_length=args.max_source_length, padding=padding, truncation=True)
+        model_inputs = tokenizer(inputs, max_length=args.max_source_length, padding=padding, truncation=True, return_tensors="pt")
 
         # Tokenize targets with the `text_target` keyword argument
-        labels = tokenizer(text_target=targets, max_length=max_target_length, padding=padding, truncation=True)
+        labels = tokenizer(text_target=targets, max_length=max_target_length, padding=padding, truncation=True, return_tensors="pt")
 
         # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
         # padding in the loss.
