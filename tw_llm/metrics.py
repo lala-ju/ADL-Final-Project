1b908a9957b7ae1e770ef1e59b5e11c015c8faed
LH104729 <lh104729@gmail.com>
2023-12-23 19:49:43 +0800

    Update

--- tw_llm/metrics.py
+++ tw_llm/metrics.py
@@ -6,15 +6,18 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--pred", type=str, required=True)
     parser.add_argument("--ans", type=str, required=True)
+    parser.add_argument("--jsonl", action="store_true")
     return parser.parse_args()
 
 def main():
     args = parse_args()
-    # pred = json.load(open(args.pred))
-    pred = []
-    with open(args.pred) as f:
-        for line in f:
-            pred.append(json.loads(line))
+    if args.jsonl:
+        pred = []
+        with open(args.pred) as f:
+            for line in f:
+                pred.append(json.loads(line))
+    else:
+        pred = json.load(open(args.pred))
     ans = json.load(open(args.ans))
     
     exact_match = 0
@@ -28,7 +31,7 @@ def main():
     false_positve = 0
     
     for p, a in zip(pred, ans):
-        assert(p["id"] == a["id"])
+        # assert(p["id"] == a["id"])
         if a["instruction"] == a["output"][0] and len(a["output"]) == 1:
             total_positive += 1
             if p["output"] == a["output"][0]:
--- tw_llm/predict.sh
+++ tw_llm/predict.sh
@@ -1,8 +1,7 @@
 python3 predict.py \
     --base_model_path ../Taiwan-LLM-7B-v2.0-chat \
-    --peft_path ./qlora_NLPCC \
-    --test_data_path ../processed_data/NLPCC2018_valid.json \
-    --output_file ./output_valid_NLPCC.json \
-    --num_beams 4\
-    --per_device_eval_batch_size 2\
+    --peft_path ./qlora_3_epoch \
+    --test_data_path ../processed_data/FCGEC_valid.json \
+    --output_file ./output_valid_FCGEC_all_data_greedy.json \
+    --per_device_eval_batch_size 4\
     --verbose
