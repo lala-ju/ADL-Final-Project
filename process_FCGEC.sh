9d72e2b161614afe1c7cd12abb6534c9773c9b0b
LH104729 <lh104729@gmail.com>
2023-12-23 23:13:38 +0800

    Added processed_data for FCGEC_train_all

--- process_FCGEC.sh
+++ process_FCGEC.sh
@@ -1,6 +1,9 @@
 python3 process_FCGEC.py --input ./data/FCGEC_train.json --output ./processed_data/FCGEC_train.json
 python3 process_FCGEC.py --input ./data/FCGEC_train.json --output ./processed_data/FCGEC_train_tw_llm.json --format tw_llm
 
+python3 process_FCGEC.py --input ./data/FCGEC_train.json --output ./processed_data/FCGEC_train_all.json --max_data 100000
+python3 process_FCGEC.py --input ./data/FCGEC_train.json --output ./processed_data/FCGEC_train_all_tw_llm.json --format tw_llm --max_data 100000
+
 python3 process_FCGEC.py --input ./data/FCGEC_valid.json --output ./processed_data/FCGEC_valid.json --valid
 
 python3 process_FCGEC.py --input ./data/FCGEC_test.json --output ./processed_data/FCGEC_test.json --format tw_llm --test
