e1d32d6f8c9678af6aac6087a9759f559e21678a
LeeYenCHieh <B10902113@ntu.edu.tw>
2023-12-23 23:04:59 +0800

    accerlate

--- roberta/inference_FCGEC.sh
+++ roberta/inference_FCGEC.sh
@@ -7,5 +7,5 @@ for((i=0;i<${epochnum};i++))
 do
     echo ${i}
     cp -f ./models/model_FCGEC01/config.json ./models/model_FCGEC01/tokenizer.json ./models/model_FCGEC01/tokenizer_config.json ./models/model_FCGEC01/epoch_${i}
-    CUDA_VISIBLE_DEVICES=${1} python3 inference.py --cuda ${1} --model ./models/model_FCGEC01/epoch_${i} --data ../processed_data/FCGEC_valid.json --output ./pd_FCGEC/pd_${i}.json
+    CUDA_VISIBLE_DEVICES=0 python3 inference.py --batch ${1} --model ./models/model_FCGEC01/epoch_${i} --input ../processed_data/FCGEC_valid.json --output ./pd_FCGEC/pd_${i}.json
 done
--- roberta/inference_NLPCC.sh
+++ roberta/inference_NLPCC.sh
@@ -7,5 +7,5 @@ for((i=0;i<${epochnum};i++))
 do
     echo ${i}
     cp -f ./models/model_NLPCC01/config.json ./models/model_NLPCC01/tokenizer.json ./models/model_NLPCC01/tokenizer_config.json ./models/model_NLPCC01/epoch_${i}
-    CUDA_VISIBLE_DEVICES=${1} python3 inference.py --cuda ${1} --model ./models/model_NLPCC01/epoch_${i} --data ../processed_data/NLPCC2018_valid.json --output ./pd_NLPCC/pd_${i}.json
+    CUDA_VISIBLE_DEVICES=1 python3 inference.py --batch ${1} --model ./models/model_NLPCC01/epoch_${i} --input ../processed_data/NLPCC2018_valid.json --output ./pd_NLPCC/pd_${i}.json
 done
