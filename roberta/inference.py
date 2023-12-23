18761697bb876ce8eb20baea57fb4ff78bc28dba
LeeYenCHieh <B10902113@ntu.edu.tw>
2023-12-23 22:31:25 +0800

    inference

---
+++ roberta/inference.py
@@ -0,0 +1,40 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import argparse
+import torch
+import json
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model")
+    parser.add_argument("--data")
+    parser.add_argument("--output")
+    parser.add_argument("--cuda")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = get_args()
+
+    with open(args.data, "r", encoding="utf8") as file:
+        all_data = json.load(file)
+    
+    device = torch.device(f'cuda:{args.cuda}')
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
+    model = AutoModelForSeq2SeqLM.from_pretrained(args.model).to(device)
+
+    result = []
+    for data in all_data:
+        temp = {"id": data["id"]}
+        inputs = tokenizer(data["instruction"], return_tensors="pt").to(device).input_ids
+        outputs = model.generate(inputs, max_new_tokens=100, num_beams=4, do_sample=False)
+        temp["output"] = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        result.append(temp)
+
+    with open(args.output, 'w') as f:
+        json.dump(result, f)
+    
+
+if __name__ == "__main__":
+    main()
