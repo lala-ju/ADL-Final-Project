0388ec96d699376d297d0bdbd10b40827a39e94a
LeeYenCHieh <B10902113@ntu.edu.tw>
2023-12-25 17:54:37 +0800

    NLPCC all

---
+++ bart/train_NLPCCall.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+CUDA_VISIBLE_DEVICES=${1} python3 trainer.py \
+    --train_file ../processed_data/NLPCC2018_train_all.json \
+    --max_source_length 512 \
+    --model_name_or_path fnlp/bart-base-chinese \
+    --tokenizer_name voidful/bart-base-chinese \
+    --text_column instruction \
+    --summary_column output \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --learning_rate 1e-4 \
+    --num_train_epochs 24 \
+    --output_dir models/model_NLPCCall03
