b805eba17ab3c3ef9342b9007df84f43d3c2f790
LH104729 <lh104729@gmail.com>
2023-12-26 22:09:16 +0800

    Added twllm download script

---
+++ tw_llm/models/download.sh
@@ -0,0 +1,8 @@
+gdown https://drive.google.com/uc?id=1OL1bpnmwtIOxNHDqBxMrxp1nJ1WL_HkY
+unzip model_FCGEC_all.zip
+
+gdown https://drive.google.com/uc?id=1k5Omyq_GuVIUw9Ns5Bhf1fJduCJXj4y3
+unzip model_NLPCC.zip
+
+gdown https://drive.google.com/uc?id=1MC2pTHH2OfUAHv9THXBw-Jdzsmm2NBoh
+unzip model_NLPCC_all.zip
