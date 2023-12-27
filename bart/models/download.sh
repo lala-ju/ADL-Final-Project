1e957fd3aba274623e64886a681facc41adc5355
LeeYenCHieh <B10902113@ntu.edu.tw>
2023-12-27 18:45:59 +0800

    bart download

---
+++ bart/models/download.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+gdown https://drive.google.com/uc?id=1zzSNs1svlfnMVUqbCgVZ-kyD92Nlcpiv
+tar zxvf model_FCGEC.tar.gz
+
+gdown https://drive.google.com/uc?id=10AP0B59ZmoQ1lwgulkC3UlBNaCvITsmi
+tar zxvf model_FCGECall.tar.gz
+
+gdown https://drive.google.com/uc?id=1i8ZBZcGLV0xpn-9ulWsCvIaNcWNkAvoU
+tar zxvf model_NLPCC.tar.gz
+
+gdown https://drive.google.com/uc?id=1EME6F1G5aBKqFj2e8jmfJiNg9Z-tDIQT
+tar zxvf model_NLPCCall.tar.gz
