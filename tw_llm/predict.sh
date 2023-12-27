a61228f0d3e49c6d3ce91577242717df732286c3
LH104729 <lh104729@gmail.com>
2023-12-27 09:58:54 +0800

    added nlpcc all

--- tw_llm/predict.sh
+++ tw_llm/predict.sh
@@ -1,6 +1,6 @@
 python3 predict.py \
     --base_model_path ../Taiwan-LLM-7B-v2.0-chat \
-    --peft_path ./qlora_FCGEC_all \
+    --peft_path ./models/model_NLPCC_all \
     --test_data_path ../processed_data/NLPCC2018_valid.json \
     --output_file ./output_valid_NLPCC_all_data_greedy.json \
     --per_device_eval_batch_size 4\
